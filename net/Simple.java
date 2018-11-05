9b48f3f10a0e7fa2f1323d4c1075a5478e9009a3
zhou.gang <zhou.gang@mgm-iot.com>
2018-11-05 13:24:30 +0800

    net/Simple.java

---
+++ net/Simple.java
@@ -0,0 +1,15 @@
+package net;
+import net.Pac;
+import net.zhou.*;
+
+public class Simple {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+	Pac p = new Pac();
+	p.get();
+	Vector v = new Vector();
+	v.get();
+    }
+
+    
+}
