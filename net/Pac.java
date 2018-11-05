24f744cbe60bccc62c5748e204923757b696a5a3
zhou.gang <zhou.gang@mgm-iot.com>
2018-11-05 13:47:20 +0800

    test modify amend

---
+++ net/Pac.java
@@ -0,0 +1,11 @@
+package net;
+
+public class Pac {
+    public static void main(String[] args) {
+        System.out.println("package");
+    }
+
+    public void get() {
+        System.out.println("hello, I'm fun");
+    }
+}
